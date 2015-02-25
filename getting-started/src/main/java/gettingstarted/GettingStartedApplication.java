3f79a0b6f06610c40783e16c804cae32642dffdc
John Thompson <fly.yamaha@gmail.com>
2015-02-24 20:00:00 -0500

    Inital add

---
+++ getting-started/src/main/java/gettingstarted/GettingStartedApplication.java
@@ -0,0 +1,12 @@
+package gettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GettingStartedApplication.class, args);
+    }
+}
---
+++ getting-started/src/test/java/gettingstarted/GettingStartedApplicationTests.java
@@ -0,0 +1,18 @@
+package gettingstarted;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = GettingStartedApplication.class)
+@WebAppConfiguration
+public class GettingStartedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
